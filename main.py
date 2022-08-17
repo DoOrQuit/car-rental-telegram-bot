62405b0fc5d91beaddcc60f5836f70a5fc0893b6
DoOrQuit <kosankos@gmail.com>
2022-08-17 13:18:22 +0300

    imported libs

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from distutils.command.config import config
 from config import TOKEN
 import aiogram
+from aiogram.dispatcher import Dispatcher
 from aiogram import types
-import os
+from aiogram.utils import executor
 
 bot = aiogram.Bot(token='TOKEN')
+dp = Dispatcher(bot)
+
+
+executor.start_polling(dp, skip_updates=True)
+
+
+
+
+
 
 
 
