272250e772207d381d325404569c0eaca8f7bebb
DoOrQuit <kosankos@gmail.com>
2022-08-18 10:07:52 +0300

    commands added. keyboard in progress

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from config import TOKEN
+from config import db_pass
+import psycopg2
 import aiogram
 from aiogram.dispatcher import Dispatcher
 from aiogram import types
 from aiogram.utils import executor
 
-bot = aiogram.Bot(token='TOKEN')
+bot = aiogram.Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+async def startup(_):
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="car_rental",
+            user="postgres",
+            password=db_pass)
+    except:
+        print('Something gone wrong during connecting to the DataBase')
+    else:
+        print('Connection to DataBase sucessfully established')
 
-executor.start_polling(dp, skip_updates=True)
+@dp.message_handler(commands = ['start', 'help'])
+async def command_start(message : types.Message):
+    bot.send_message(message.from_user.id, )
+
+async def command_help(message : types.Message):
+    bot.send_message(message.from_user.id, )
+
+
+@dp.message_handler()
+async def echo_reply(message : types.Message):
+    await message.reply(f"Hello! I'm Your assistant. I like to chat with You, but please use command buttons to navigate.")
 
 
 
@@ -19,3 +42,7 @@ executor.start_polling(dp, skip_updates=True)
 
 
 
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
