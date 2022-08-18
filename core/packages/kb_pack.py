4bcd81a7ac7bcca6897fbaec67b238163ceb74a2
DoOrQuit <kosankos@gmail.com>
2022-08-18 14:19:04 +0300

    revised start command and reply for some commands like "rent"

--- core/packages/kb_pack.py
+++ core/packages/kb_pack.py
@@ -1,11 +1,21 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 
-start_button = KeyboardButton('/start')
-help_button = KeyboardButton('/help')
-contacts_button = KeyboardButton('/contacts')
+
+"""Main buttons"""
+start_bt = KeyboardButton('/start')
+help_bt = KeyboardButton('/help')
+contacts_bt = KeyboardButton('/contacts')
+car_rent_bt = KeyboardButton('Rent \U0001F698')
 
 main_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
-main_buttons.add(start_button).add(help_button).add(contacts_button)
+main_buttons.add(car_rent_bt).add(help_bt).add(contacts_bt)
+
+
+"""Help Buttons"""
+share_phone = KeyboardButton("Call me Back \U0001F4DE", request_contact=True)
+assistant_contact = KeyboardButton("Get an assistant number \U0001F4DE")
+help_buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+help_buttons.row(share_phone, assistant_contact)
 
 
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from cgitb import text
+from email import message
 from config import TOKEN
 from config import db_pass
+from datetime import date, datetime
 import psycopg2
 import aiogram
 from aiogram.dispatcher import Dispatcher
 from aiogram import types
 from aiogram.utils import executor
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 from core.packages import kb_pack as kb
 
 
 bot = aiogram.Bot(token=TOKEN)
 dp = Dispatcher(bot)
+current_time = datetime.now().strftime("%H%M")
 
 #async def startup(_):
     #try:
@@ -23,24 +26,38 @@ dp = Dispatcher(bot)
     #else:
         #print('Connection to DataBase sucessfully established')
 
-@dp.message_handler(commands = ['start', 'help'])
+@dp.message_handler(commands = ['start'])
 async def command_start(message : types.Message):
-    await bot.send_message(message.from_user.id, "Fine. Let's start!", reply_markup= kb.main_buttons)
+    if 1200 > int(current_time) > 0000 :
+        await bot.send_message(message.from_user.id, f"Good morning, {message.from_user.first_name}."
+                                                    " Please, press one of the buttons below \U00002B07", reply_markup= kb.main_buttons)
+    elif 1600 > int(current_time) > 1200 :
+        await bot.send_message(message.from_user.id, f"Good afternoon, {message.from_user.first_name}."
+                                                    " Please, press one of the buttons below \U00002B07", reply_markup= kb.main_buttons)
+    elif 2359 > int(current_time) > 1600 :
+        await bot.send_message(message.from_user.id, f"Good evening, {message.from_user.first_name}."
+                                                    " Please, press one of the buttons below \U00002B07", reply_markup= kb.main_buttons)
 
+@dp.message_handler(commands = ['help'])
 async def command_help(message : types.Message):
-    await bot.send_message(message.from_user.id, "You pressed a Help button", reply_markup= kb.main_buttons)
+    await bot.send_message(message.from_user.id, "Don't worry. We'll assist You with any problem", reply_markup= kb.help_buttons)
 
 @dp.message_handler(commands = ['contacts'])
 async def command_contacts(message : types.Message):
-    await bot.send_message(message.from_user.id, "b.125 Myra Street, Kyiv\nPhone Number : +380123456789"
-                                                "\nemail: test@rentals.ua", reply_markup=kb.main_buttons)
+    await bot.send_message(message.from_user.id, "Office address: b.125 Myra Street, Kyiv\nPhone Number: +380123456789"
+                                                "\nEmail: test@rentals.ua", reply_markup=kb.main_buttons)
+
+@dp.message_handler(lambda message : 'rent' in message.text.lower())
+async def text_rent(message : types.Message):
+    await message.reply("Sure! We'll find a perfect car for You!")
 
 
 
 
 @dp.message_handler()
 async def echo_reply(message : types.Message):
-    await message.reply(f"Hello! I'm Your assistant. I like to chat with You, but please use command buttons to navigate.")
+    await message.reply("Hello! I'm Your assistant. Probably I didn't understand you." 
+                        " Please repeat Your question.")
 
 
 
