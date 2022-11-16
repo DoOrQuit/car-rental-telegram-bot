6901d17d451c7243b07ad24d76ac5b5fd7ea4775
Antony_K <kosankos@gmail.com>
2022-11-16 19:24:22 +0200

    Car_info func improved to send a more "humanlike" message to an user instead of dict-like brackets.

--- core/car_info_img/model_info.py
+++ core/car_info_img/model_info.py
@@ -1,32 +1,31 @@
 import psycopg2
-import pprint
 from config import db_pass
 
 
-def car_info(category:str, brand:str, model:str):
+def car_info(category: str, brand: str, model: str):
     """
-    Function fetches from DB the information regarding particular car specs such as : Transmission type, Fuel consumption etc.
+    Function fetches from DB the information regarding particular car specs
+    such as : Transmission type, Fuel consumption etc.
     """
 
     try:
-        connect = psycopg2.connect(database = 'car_rental', 
-                                    user = 'postgres', 
-                                    password = db_pass)
+        connect = psycopg2.connect(database='car_rental', user='postgres', password=db_pass)
         curs = connect.cursor()
 
-        if category in ('suv', 'minivan'):  # 'suv' and 'minivan' category placed in IF block as they required different query from DB
-            
+        # 'suv' and 'minivan' category placed in IF block as they required different query from DB
+        if category in ('suv', 'minivan'):
+
             curs.execute("""SELECT brand, model, price, fuel, motor::float as motor, transmission,
                             air_conditioning, fuel_consumption_100km::float as fuel_consumption_100km, seating_capacity
                             FROM fleet
                             WHERE sub_category = %s and brand = %s and model = %s""", (category, brand, model))
         
             fetched_list = (list(curs.fetchall()[0]))
-            header = [var[0].capitalize() for var in curs.description]
-            car_info = dict(zip(header, fetched_list))  # pprint is used to bring the car information to the client side as 
-                                                        # readible message in Telegram chat
-            result = pprint.pformat(car_info, width=1, sort_dicts=False)
-            return result
+            titles = [var[0].capitalize() for var in curs.description]
+            message = ''
+            for (params, header) in zip(fetched_list, titles):
+                message += f'{header}:   {params}\n'
+            return message
         
         else:
 
@@ -36,13 +35,12 @@ def car_info(category:str, brand:str, model:str):
                             WHERE category = %s and brand = %s and model = %s""", (category, brand, model))
 
             fetched_list = (list(curs.fetchall()[0]))
-            header = [var[0].capitalize() for var in curs.description]
-            car_information = dict(zip(header, fetched_list))
-            result = pprint.pformat(car_information, width=1, sort_dicts=False)
-            return result
+            titles = [var[0].capitalize() for var in curs.description]
+            message = ''
+            for (params, header) in zip(fetched_list, titles):
+                message += f'{header}:   {params}\n'
+            return message
 
-    except:
-        print('oops. DB die')
     finally:
         curs.close()
         connect.close()
